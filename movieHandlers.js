78614ffa989634d7a5b9d38032b6598974040f9b
Charlie FEIX <contact@charliefeix.fr>
2023-05-22 16:48:34 +0200

    Add delete route

--- movieHandlers.js
+++ movieHandlers.js
@@ -49,7 +49,7 @@ const updateMovie = (req, res) => {
     const { title, director, year, color, duration } = req.body;
 
     database
-        .query("update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?", [title, director, year, color, duration, id])
+        .query("UPDATE movies SET title = ?, director = ?, year = ?, color = ?, duration = ? WHERE id = ?", [title, director, year, color, duration, id])
         .then(([result]) => {
             if (result.affectedRows === 0) {
                 res.status(404).send("Not Found");
@@ -63,9 +63,27 @@ const updateMovie = (req, res) => {
         });
 };
 
+const deleteMovie = (req, res) => {
+    const id = parseInt(req.params.id);
+    database
+        .query("DELETE FROM movies WHERE id = ?", [id])
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(200);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error deleting the movie");
+        });
+};
+
 module.exports = {
     getMovies,
     getMovieById,
     postMovie,
     updateMovie,
+    deleteMovie,
 };
